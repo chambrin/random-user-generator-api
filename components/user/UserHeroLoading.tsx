b0066b528f8fc17f7b92f5e82543353d1009c058
chambrin <achambrin@outlook.fr>
2023-09-06 11:57:28 +0200

    🚧 Hero section

--- components/user/UserHeroLoading.tsx
+++ components/user/UserHeroLoading.tsx
@@ -1,51 +1,121 @@
 'use client'
-import { useState, useEffect } from 'react'
-import { FaUser, FaEnvelope, FaHome, FaPhoneAlt } from 'react-icons/fa'
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import { FaUser, FaEnvelope, FaHome, FaPhoneAlt } from 'react-icons/fa';
 
 export default function CardUserHero() {
-    const [tab, setTab] = useState('info')
-    const [user, setUser] = useState(null)
+    const [tab, setTab] = useState('info');
+    const [user, setUser] = useState(null);
+    const [x , setX] = useState(0);
+    const [y, setY] = useState(0);
 
+    // get the user data
     useEffect(() => {
         fetch('http://localhost:3002/api/user/1')
             .then(response => response.json())
             .then(data => setUser(data))
-            .catch(error => console.log(error))
+            .catch(error => console.log(error));
     }, [])
 
-    if (!user) return <div>Loading...</div>
+    // detect the mousemove and update x and y
+    useEffect(() => {
+        const handleMouseMove = (e) => {
+            setX((e.clientY / window.innerHeight - 0.5) * 40);
+            setY((e.clientX / window.innerWidth - 0.5) * -40);
+        };
+        window.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
+
+    if (!user) return <div className='loader bw'>
+        <div className='flow-cross' />
+    </div>;
+
+    // Animation settings
+    const floatVariants = {
+        hidden: { y: -2 },
+        show: {
+            y: 2,
+            transition: {
+                repeat: Infinity,
+                repeatType: 'reverse',
+                duration: .600
+            },
+        },
+    };
 
     return (
-        <div className='UserGeneratorHero flex flex-col items-center justify-center p-5 gap-6'>
-            <div className='w-1/2'>
+        <div
+
+            className='UserGeneratorHero flex flex-col items-center justify-center p-6 gap-6 bg-gradient-to-r from-blue-500 to-purple-500 rounded-lg shadow-lg'
+        >
+            <motion.div
+                variants={floatVariants}
+                initial="hidden"
+                animate="show"
+                className='w-1/2 p-5'
+            >
                 <img
                     className='rounded-full max-w-full h-auto object-cover mx-auto ring-2 ring-[#5142FC]'
                     src={user.user_image_url}
                     alt="User avatar"
                 />
+            </motion.div>
+
+            <div className=''>
+                <p className='text-2xl font-bold text-center'>{user.name}</p>
             </div>
 
-
-            <div className='bg-white bg-opacity-20 rounded-lg shadow-lg p-4'>
-                <p className=''>{user.name}</p>
+            <div className='px-6 py-4 text-white space-x-2 flex justify-between'>
+                <motion.button
+                    variants={floatVariants}
+                    initial="hidden"
+                    animate="show"
+                    className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full'
+                    onClick={() => setTab('info')}
+                >
+                    <FaUser />
+                </motion.button>
+                <motion.button
+                    variants={floatVariants}
+                    initial="hidden"
+                    animate="show"
+                    className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full'
+                    onClick={() => setTab('email')}
+                >
+                    <FaEnvelope />
+                </motion.button>
+                <motion.button
+                    variants={floatVariants}
+                    initial="hidden"
+                    animate="show"
+                    className='bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded-full'
+                    onClick={() => setTab('address')}
+                >
+                    <FaHome />
+                </motion.button>
+                <motion.button
+                    variants={floatVariants}
+                    initial="hidden"
+                    animate="show"
+                    className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full'
+                    onClick={() => setTab('phone')}
+                >
+                    <FaPhoneAlt />
+                </motion.button>
             </div>
 
-            
-            <div className='px-6 py-4 text-black'>
-                <div className='space-x-2'>
-                    <button onClick={() => setTab('info')}><FaUser /></button>
-                    <button onClick={() => setTab('email')}><FaEnvelope /></button>
-                    <button onClick={() => setTab('address')}><FaHome /></button>
-                    <button onClick={() => setTab('phone')}><FaPhoneAlt /></button>
-                </div>
-                {tab === 'info' && <p>{user.name}</p>}
-                {tab === 'email' && <p>{user.email}</p>}
-                {tab === 'address' && <p>{user.address}</p>}
-                {tab === 'phone' && <p>{user.phone_number}</p>}
+            <div className=' bg-opacity-75 rounded-lg shadow-lg p-4 m-2 text-center'>
+                {tab === 'info' && <p className='text-xl font-semibold'>{user.name}</p>}
+                {tab === 'email' && <p className='text-xl font-semibold'>{user.email}</p>}
+                {tab === 'address' && <p className='text-xl font-semibold'>{user.address}</p>}
+                {tab === 'phone' && <p className='text-xl font-semibold'>{user.phone_number}</p>}
             </div>
+
         </div>
+    );
 
-
-
-    )
 }
